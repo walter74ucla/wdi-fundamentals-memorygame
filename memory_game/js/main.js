e629ac25da835def25737908e74dfab9c4995d9e
Walter Sylvester <walter_sylvester@msn.com>
2019-09-06 22:34:34 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -35,15 +35,27 @@ function checkForMatch() {
 	}
 };
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay.length > 1) {
 	checkForMatch();
+	}
 };
-flipCard(0);
-flipCard(2);
 
+function createBoard() {
+	for(i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
 
 
